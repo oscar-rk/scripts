3617ef6295f1868b1e245f77d31d191c44c4afbf
oscar-rk <23240095+oscar-rk@users.noreply.github.com>
2022-09-10 13:56:04 +0200

    Improved portScan.sh shell script

--- Infosec/Recon/portScan.sh
+++ Infosec/Recon/portScan.sh
@@ -5,12 +5,21 @@
 # Usage:
 # ./portScan.sh <ip-address>
 
-if [ $1 ]; then
-	ip_address=$1
-	for port in $(seq 1 65535); do
-		timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null && echo "[*] Port $port OPEN" &
-	done; wait
-else
-	echo -e "\n [*] Usage: ./portScan.sh <ip-address>\n"
+# User exit handling
+function ctrl_c(){
+   echo -e "[!] Exiting script ...\n"   
+   exit 1
+}
+
+# User exit catching
+trap ctrl_c INT
+
+# Args check
+if [ "$#" -ne 1 ]; then
+	echo -e "\n [!] Usage: $0 <ip-address>\n"
 	exit 1
 fi
+
+for port in $(seq 1 65535); do
+	timeout 1 bash -c "echo '' > /dev/tcp/$1/$port" 2>/dev/null && echo "[+] Port $port OPEN" &
+done; wait
